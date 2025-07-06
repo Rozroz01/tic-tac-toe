ae40878f2134516d143c8f8344497ae939eca3a6
Rozy Kassab <rosy.kassab.99@gmail.com>
2025-07-06 22:42:28 +0200

    Add checking methods for a win or tie

--- tictactoe.py
+++ tictactoe.py
@@ -24,6 +24,52 @@ def playerInput(board):
     else:
         print("\nOops! this spot is already occupied. Try another number!\n")
 
+# Check for win or tie
+
+
+def checkHorisontal(board):
+    global winner  # the global keyword allows us to modify the global variable winner, so if we make changes in the scope of this function, the winner variable will change in the scope of the entire file.
+    if board[0] == board[1] == board[2] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
+
+
+def checkVertical(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
+
+
+def checkDiagonal(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != "-":
+        winner = board[2]
+        return True
+
+
+def checkTie(board):
+    global gameRunning
+    if "-" not in board:
+        printBoard(board)
+        print("It's a tie!")
+        gameRunning = False
+
 
 while gameRunning:
     printBoard(board)
